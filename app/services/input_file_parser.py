67c38d295a9f29299c9a70e784cb07da789dd13e
ahmedAbouismail <s0565952@htw-berlin.de>
2024-11-12 20:14:46 +0100

    Edit input_file_parser.py

--- app/services/input_file_parser.py
+++ app/services/input_file_parser.py
@@ -1,5 +1,6 @@
 from utils.logger import get_logger
 from typing import BinaryIO, Dict, Any, Optional
+from core.config import settings
 import json
 
 
@@ -7,6 +8,7 @@ class InputFileParser:
     """
     Service class for parsing input files
     """
+
     def __init__(self, text_file: BinaryIO, json_file: BinaryIO):
         """
         :param text_file: Uploaded text file
@@ -16,6 +18,30 @@ class InputFileParser:
         self.text_file = text_file
         self.json_file = json_file
         self.logger.info("Initialized InputFileParser")
+
+    def _check_json_obj_depth(self, depth: int) -> bool:
+        """
+        Checks if the json depth is valid
+        :param depth: depth of the json object
+        :return: True if valid, False otherwise
+        """
+        if depth <= settings.json_depth and depth != 0:
+            return True
+        else:
+            return False
+
+    def _calculate_json_depth(self, json_dict: Dict[str, Any]) -> int:
+        """
+        Checks the depth of the JSON Obj
+        :param json_dict:Uploaded json file
+        :return: depth of the JSON Obj
+        """
+        if isinstance(json_dict, dict):
+            return 1 + max((self._calculate_json_depth(value) for value in json_dict.values()), default=0)
+        elif isinstance(json_dict, list):
+            return 1 + max((self._calculate_json_depth(item) for item in json_dict), default=0)
+        else:
+            return 0
 
     def parse_json(self) -> Optional[Dict[str, Any]]:
         """
@@ -28,7 +54,15 @@ class InputFileParser:
             json_data = json.loads(content)
             if not isinstance(json_data, dict):
                 self.logger.error("JSON root should be an object.")
-            return json_data
+
+            depth = self._calculate_json_depth(json_data)
+            self.logger.info(f"Response structure depth = {depth}")
+            if self._check_json_obj_depth(depth):
+                return json_data
+            else:
+                self.logger.error(
+                    f"Depth of the response structure {depth} is invalid.")
+                raise Exception(f"Depth of the response structure {depth} is invalid. Valid depth is {settings.json_depth}")
         except json.decoder.JSONDecodeError:
             self.logger.error("Invalid uploaded file content")
         return None
